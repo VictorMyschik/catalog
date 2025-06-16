daed6dc32105439fbde0f0b6a36697370aa3ef7d
Viktar Myshchyk <v.myshchik@apicworld.com>
2025-06-16 12:04:22 +0200

    n/s

--- config/logging.php
+++ config/logging.php
@@ -63,6 +63,8 @@ return [
             'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
             'replace_placeholders' => true,
+            'max_files' => env('LOG_SINGLE_MAX_FILES', 5),
+            'max_size' => env('LOG_SINGLE_MAX_SIZE', 5),
         ],
 
         'daily' => [
